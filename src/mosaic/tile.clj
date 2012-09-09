a7147274a362a08b54bf0b64a79ee2c052dc9901
Joseph Burnett <josephburnett79@gmail.com>
2012-09-09 14:11:10 -0700

    Generate tiles with (save-tiles (gen-tiles 100 (test-image)) "test/resources/")

--- src/mosaic/tile.clj
+++ src/mosaic/tile.clj
@@ -1,28 +1,42 @@
 (ns mosaic.tile
   (import java.io.File)
-  (import java.awt.Color)
-  (import java.awt.image.BufferedImage)
-  (import java.awt.image.Raster)
   (import javax.imageio.ImageIO)
-  (import com.mortennobel.imagescaling.ResampleOp)
-  (import java.awt.Rectangle))
+  (import java.awt.image.BufferedImage)
+  (import com.mortennobel.imagescaling.ResampleOp))
 
-(defn test-mosaic []
+(defn test-image []
   (ImageIO/read
    (File. "test/resources/image.jpg")))
 
 (defn rescale [x y ^BufferedImage b]
   (. (ResampleOp. x y) (filter b nil)))
 
-(defn tile-rects [n x y]
-  "divide dimensions x,y into Rectangles of size n
-   omitting regions that are not complete tiles"
-  (for [a (range 0 (- x n -1) n) b (range 0 (- y n -1) n)]
-    (Rectangle. a b n n)))
+(defn grid
+  "Divide region 0,0,x,y into grid squares of size n.
+   Partial grids squares are omitted. Optional parameter
+   s sets the step size to allow for overlapping squares."
+  ([n x y] (grid n x y n))
+  ([n x y s]
+     (for
+	 [a (range 0 (- x n -1) s)
+	  b (range 0 (- y n -1) s)]
+       [a,b,(+ a n),(+ b n)])))
 
-(defn tile-rasters [n ^BufferedImage b]
-  "divide buffered image into rasters of size n"
-  (let [h (.getHeight b)
-	w (.getWidth b)]
-    (map #(.getData b %) (tile-rects n w h))))
+(defn gen-tiles [n ^BufferedImage b]
+  "Generate tiles of size n from image b.
+   Returns a list of BufferedImages."
+  (map #(.getSubimage b (first %) (second %) n n)
+       (grid 100 (.getWidth b) (.getHeight b))))
+
+(defn save-image [^BufferedImage b ^String f]
+  "Save a BufferedImage to filename f."
+  (ImageIO/write b "jpg" (File. f)))
+
+(defn tile-names [^String base]
+  "Infinite sequence of tile names."
+  (map #(str base % ".jpg") (range)))
+
+(defn save-tiles [tiles ^String d]
+  "Save tiles (list of BufferedImages) to directory d."
+  (map #(save-image %1 %2) tiles (tile-names d)))
 
