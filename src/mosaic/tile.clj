87205a0b9fe34f7427a0260df6b87904354bbfdc
Joseph Burnett <josephburnett79@gmail.com>
2012-09-08 22:47:43 -0700

    I can get Rasters for tile regions in an image

--- src/mosaic/tile.clj
+++ src/mosaic/tile.clj
@@ -4,7 +4,8 @@
   (import java.awt.image.BufferedImage)
   (import java.awt.image.Raster)
   (import javax.imageio.ImageIO)
-  (import com.mortennobel.imagescaling.ResampleOp))
+  (import com.mortennobel.imagescaling.ResampleOp)
+  (import java.awt.Rectangle))
 
 (defn test-mosaic []
   (ImageIO/read
@@ -12,3 +13,16 @@
 
 (defn rescale [x y ^BufferedImage b]
   (. (ResampleOp. x y) (filter b nil)))
+
+(defn tile-rects [n x y]
+  "divide dimensions x,y into Rectangles of size n
+   omitting regions that are not complete tiles"
+  (for [a (range 0 (- x n -1) n) b (range 0 (- y n -1) n)]
+    (Rectangle. a b n n)))
+
+(defn tile-rasters [n ^BufferedImage b]
+  "divide buffered image into rasters of size n"
+  (let [h (.getHeight b)
+	w (.getWidth b)]
+    (map #(.getData b %) (tile-rects n w h))))
+
