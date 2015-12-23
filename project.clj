afc91ed179ccf3a0e8af64375cbfeb371fc2a0ec
Joseph Burnett <josephburnett79@gmail.com>
2015-12-23 10:11:43 -0800

    Examples and profiling.

--- project.clj
+++ project.clj
@@ -4,4 +4,9 @@
                  [org.clojure/tools.cli "0.2.2"]
                  [com.mortennobel/java-image-scaling "0.8.5"]
                  [clj-kdtree "1.2.0" :exclusions [org.clojure/clojure]]]
-  :main mosaic.main)
+  :main mosaic.main
+  :jvm-opt ["-Xmx512m"
+            "-Dcom.sun.management.jmxremote"
+            "-Dcom.sun.management.jmxremote.ssl=false"
+            "-Dcom.sun.management.jmxremote.authenticate=false"
+            "-Dcom.sun.management.jmxremote.port=43210"])
--- test/resources/benchmark.sh
+++ test/resources/benchmark.sh
@@ -1,3 +1,3 @@
 # From project root: `source test/resources/benchmark.sh`
 
-lein run --output "/tmp/mosaic-benchmark.jpg" --tile-size 10 --step-size 10 --width 50 --samples 3 "test/resources/image.jpg" "test/resources/image.jpg"
+lein run --output "/tmp/lcp-demo.jpg" --tile-size 20 --step-size 20 --width 100 --samples 3 ~/Downloads/demo/dragonfly.jpg ~/Downloads/demo/tiles/*
